c81ed80a1ab72e95539b8b4ef6f71790b5958b30
ardit203 <arditselmani203@gmail.com>
2026-03-02 00:58:11 +0100

    init

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from py import polish_data
 if __name__ == "__main__":
     polish_data.main()
 
-    # PORT = 9999
-    # Handler = http.server.SimpleHTTPRequestHandler
-    #
-    # with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    #     webbrowser.open(f"http://localhost:{PORT}/index.html")
-    #     httpd.serve_forever()
+    PORT = 9999
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        webbrowser.open(f"http://localhost:{PORT}/index.html")
+        httpd.serve_forever()
